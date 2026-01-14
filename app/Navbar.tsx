80f70d01e8151ea79549c98909db2c784af3af05
Bko-hashing <bernardko1203@gmail.com>
2026-01-13 20:57:38 -0500

    navbar goal update

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Navbar: React.FC = () => (
   <nav className="w-full bg-blue-600 shadow text-white px-6 py-4 flex justify-between items-center">
     <span className="text-3xl font-bold" style={{ fontFamily: '"Dancing Script", cursive' }}>
-      <Link href="/" className="hover:underline px-2">Bernard Ko</Link>, An aspiring software engineer
+      <Link href="/" className="hover:underline px-2">Bernard Ko</Link>, An aspiring Quant Developer
       </span>
     <div className="space-x-6 pr-8">
       <Link href="/" className="hover:underline px-2">Home</Link>
